3930c794c2d0fbc306fb7ae1003600b8ac96fe3b
rosjerry <rostomi1306@gmail.com>
2025-09-03 18:01:29 +0400

    feat: first using useTick, spining animation implement

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Application, extend } from "@pixi/react";
-import { Container, Sprite, Graphics, Texture, Assets, Text } from "pixi.js";
-import { useEffect, useState } from "react";
+import { Container, Sprite, Graphics, Text } from "pixi.js";
+import Scene from "./Scene";
 
 extend({
   Container,
@@ -10,21 +10,6 @@ extend({
 });
 
 function App() {
-  const [uiTexture, setUiTexture] = useState<Texture | null>(null);
-
-  useEffect(() => {
-    let isMounted = true;
-    Assets.load("/assets/main/ui.png").then((res) => {
-      if (!isMounted) return;
-      const tex =
-        res instanceof Texture ? res : Texture.from("/assets/main/ui.png");
-      setUiTexture(tex);
-    });
-    return () => {
-      isMounted = false;
-    };
-  }, []);
-
   return (
     <Application
       backgroundColor={0x922724}
@@ -35,49 +20,7 @@ function App() {
         align: "center",
       }}
     >
-      <pixiContainer x={0} y={0}>
-        <pixiGraphics
-          x={20}
-          y={20}
-          draw={(g) => {
-            g.clear();
-            g.fill(0x00ff00);
-            g.rect(0, 0, 120, 80);
-            g.fill();
-          }}
-          eventMode="static"
-          onMouseEnter={() => {
-            console.log("gr clck");
-          }}
-        />
-
-        {uiTexture && (
-          <pixiSprite
-            eventMode="static"
-            texture={uiTexture}
-            x={600}
-            y={400}
-            anchor={{ x: 0, y: 0 }}
-            width={200}
-            height={200}
-            rotation={Math.PI / 3}
-            onMouseEnter={() => {
-              console.log("clicked");
-            }}
-          />
-        )}
-
-        <pixiText
-          text="Hello World!"
-          x={700}
-          y={800}
-          style={{
-            fontSize: 32,
-            fill: "#ffffff",
-            fontFamily: "Arial",
-          }}
-        />
-      </pixiContainer>
+      <Scene />
     </Application>
   );
 }
---
+++ src/Scene.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import { useTick } from "@pixi/react";
+import { Texture, Assets } from "pixi.js";
+
+function Scene() {
+  const [uiTexture, setUiTexture] = useState<Texture | null>(null);
+  const [isSpinning, setIsSpinning] = useState(false);
+  const [rotation, setRotation] = useState(0);
+
+  useEffect(() => {
+    let isMounted = true;
+    Assets.load("/assets/main/ui.png").then((res) => {
+      if (!isMounted) return;
+      const tex =
+        res instanceof Texture ? res : Texture.from("/assets/main/ui.png");
+      setUiTexture(tex);
+    });
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  useTick(() => {
+    if (isSpinning) {
+      setRotation((r) => r + 0.05);
+    }
+  });
+
+  return (
+    <pixiContainer x={0} y={0}>
+      <pixiGraphics
+        x={100}
+        y={200}
+        draw={(g) => {
+          g.clear();
+          g.fill(0x00ff00);
+          g.rect(0, 0, 120, 80);
+          g.fill();
+        }}
+        eventMode="static"
+        rotation={rotation}
+        onMouseEnter={() => setIsSpinning(true)}
+        onMouseLeave={() => setIsSpinning(false)}
+      />
+
+      {uiTexture && (
+        <pixiSprite
+          eventMode="static"
+          texture={uiTexture}
+          x={600}
+          y={400}
+          anchor={{ x: 0, y: 0 }}
+          width={200}
+          height={200}
+          rotation={rotation}
+          onMouseEnter={() => setIsSpinning(true)}
+          onMouseLeave={() => setIsSpinning(false)}
+        />
+      )}
+
+      <pixiText
+        text="Hello World!"
+        x={100}
+        y={100}
+        onMouseEnter={() => setIsSpinning(true)}
+        onMouseLeave={() => setIsSpinning(false)}
+        rotation={rotation}
+        style={{
+          fontSize: 32,
+          fill: "#ffffff",
+          fontFamily: "Arial",
+        }}
+      />
+    </pixiContainer>
+  );
+}
+
+export default Scene;
