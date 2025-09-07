1d412649edfcd6417787590e4c03d402294e372e
rosjerry <rostomi1306@gmail.com>
2025-09-07 19:20:29 +0400

    feat: create backend alike data structure for local

---
+++ src/api/types.ts
@@ -0,0 +1,20 @@
+export type BoardCell = number | 'bonus';
+
+export interface GameStateResponse {
+  balance: number;
+  dice_result: [number, number] | null;
+  last_prize_won: number | null;
+  available_to_spin: boolean;
+  bonus_mode_board: BoardCell[] | null;
+  bonus_mode: boolean;
+  freespin_amount: number;
+  regular_mode_board: BoardCell[];
+}
+
+export const getActiveBoard = (state: GameStateResponse): BoardCell[] => {
+  return state.bonus_mode && state.bonus_mode_board
+    ? state.bonus_mode_board
+    : state.regular_mode_board;
+};
+
+
